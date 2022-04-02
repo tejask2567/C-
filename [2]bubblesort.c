0f601dfc11a0ca5f62dc8c19ba588da296511536
tejask2567 <101116628+tejask2567@users.noreply.github.com>
2022-04-02 20:26:50 +0530

    Bubble sort in C

--- [2]bubblesort.c
+++ [2]bubblesort.c
@@ -11,7 +11,7 @@ int main() {
 
   for(i=0;i<n;i++){
     for(j=0;j<n-1-i;j++){
-      if(a[j]>a[j+1]){
+      if(a[j]>a[j+1]){ //For descending replace with (a[j]<a[j+1])
         temp=a[j];
         a[j]=a[j+1];
         a[j+1]=temp;
